927971bbf6e26ac597926a7acc1a49699415ec3d
shohta <shohtywinter@gmail.com>
2025-11-08 14:58:31 +0900

    change connection to the class, not the function.

--- connection.py
+++ connection.py
@@ -21,31 +21,30 @@ def tcp_conn(command):
             print("⚠️  No response (timeout)")
             return None
     return response
+class PLCConnection:# Modified the function above to class
+    def __init__(self, ip="192.168.0.2", port=12289, timeout=5.0):
+        self.ip = ip
+        self.port = port
+        self.timeout = timeout
+        self.socket = None
 
-# class PLCConnection:# Modified the function above to class
-#     def __init__(self, ip="192.168.0.2", port=12289, timeout=5.0):
-#         self.ip = ip
-#         self.port = port
-#         self.timeout = timeout
-#         self.socket = None
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(self.timeout)
+        self.socket.connect((self.ip, self.port))
+        print(f"✓ Connected to {self.ip}:{self.port}")
 
-#     def connect(self):
-#         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         self.socket.settimeout(self.timeout)
-#         self.socket.connect((self.ip, self.port))
-#         print(f"✓ Connected to {self.ip}:{self.port}")
+    def send(self, command: str):
+        """Send an ASCII command, return response as bytes"""
+        self.socket.sendall(command.encode())
+        print(f"Sent     : {command.encode()}")
 
-#     def send(self, command: str):
-#         """Send an ASCII command, return response as bytes"""
-#         self.socket.sendall(command.encode())
-#         print(f"Sent     : {command.encode()}")
+        response = self.socket.recv(1024)
+        # print(f"Response : {response.decode(errors='ignore').strip()}")
+        # print(f"Response : {response.decode()}")
+        return response
 
-#         response = self.socket.recv(1024)
-#         # print(f"Response : {response.decode(errors='ignore').strip()}")
-#         # print(f"Response : {response.decode()}")
-#         return response
-
-#     def close(self):
-#         if self.socket:
-#             self.socket.close()
-#             print("✗ Connection closed.")
+    def close(self):
+        if self.socket:
+            self.socket.close()
+            print("✗ Connection closed.")
